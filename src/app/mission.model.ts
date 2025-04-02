5ba26593c419316ac14e170ddac3f53b50445961
Mohmadsahil Shaikh <101413469@georgebrown.ca>
2025-04-02 16:41:00 -0400

    reverting the fixes

--- src/app/mission.model.ts
+++ src/app/mission.model.ts
@@ -3,22 +3,15 @@ export interface Mission {
     mission_name: string;
     launch_year: string;
     details: string;
-    launch_success?: boolean;
-
     rocket: {
       rocket_name: string;
       rocket_type: string;
-      first_stage?: {
-        cores?: {
-          land_success?: boolean;
-        }[];
-      };
     };
-
     links: {
       mission_patch_small: string;
       article_link: string;
       wikipedia: string;
       video_link: string;
     };
+
   }
